ffd881a5c8b3c0bf4f466b0b2ad3f5806da6a6ed
DW31 <mmendizabal1999@gmail.com>
2020-11-27 13:50:01 +0100

    segunda copia

--- prueba.java
+++ prueba.java
@@ -4,22 +4,13 @@ package entornos;
 public class prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Give me a number");
+		System.out.println("Dame un numero");
 		int usernumber = Console.readInt();
 		int number = usernumber;
-		int div = 2;
-		System.out.print(number + "=");
-		while (number != 1) {
-			if (number % div == 0) {
-				number = number / div;
-				System.out.print(div);
-				if (number!=1) {
-					System.out.print("*");
-				}
-			}
-			else {
-				div++;
-			}
+		
+		for (int i = number; i >= 0; i--) {
+			System.out.print(number + "  ");
+			number *= i;
 		}
 	}
 
